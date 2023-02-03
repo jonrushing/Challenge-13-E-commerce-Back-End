ee9126bdc2d8d14da368087deceee38e1173781b
Jon <Jon@Minettes-MacBook-Pro-2.local>
2023-02-03 00:06:45 -0500

    almost done

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection.js');
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3050;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
