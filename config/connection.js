6ce3b3b76bcaf4707c40fa7ec9403f60e4ae1b61
Jon <Jon@Minettes-MacBook-Pro-2.local>
2023-02-19 04:09:01 -0500

    fixed some connection issues

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
-
 const Sequelize = require('sequelize');
-console.log(process.env);
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3050
+  });
+}
 
 module.exports = sequelize;
